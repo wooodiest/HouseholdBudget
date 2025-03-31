67c7c665876a3bdbb76f55025861cb0b8ec31ced
wooodiest <michalkuchnickiisc@gmail.com>
2025-03-31 19:29:18 +0200

    Added transaction category

---
+++ src/Core/Models/Category.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseholdBudget.src.Core.Models
+{
+    public enum CategoryType
+    {
+        Income,
+        Expense
+    }
+
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public CategoryType Type { get; set; }
+
+        public Category(int id, string name, CategoryType type)
+        {
+            Id   = id;
+            Name = name;
+            Type = type;
+        }
+
+        public void EditCategory(string name, CategoryType type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, Name: {Name}, Type: {Type}";
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
