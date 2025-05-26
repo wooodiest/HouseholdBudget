90cc116278209e2ab8986a8c58118ebd12f2fe37
wooodiest <michalkuchnickiisc@gmail.com>
2025-05-26 20:25:44 +0200

    Fized typo with transaction deleted event

--- HouseholdBudget.Core/Events/Transactions/TransactionDeleted.cs
+++ HouseholdBudget.Core/Events/Transactions/TransactionDeleted.cs
@@ -1,8 +1,10 @@
-﻿namespace HouseholdBudget.Core.Events.Transactions
+﻿using HouseholdBudget.Core.Models;
+
+namespace HouseholdBudget.Core.Events.Transactions
 {
     /// <summary>
     /// Event raised when an existing transaction is deleted.
     /// </summary>
-    /// <param name="transactionId">The identifier of the deleted transaction.</param>
-    public record TransactionDeleted(Guid transactionId) : ITransactionEvent;
+    /// <param name="Transaction">The transaction object being deleted.</param>
+    public record TransactionDeleted(Transaction Transaction) : ITransactionEvent;
 }
