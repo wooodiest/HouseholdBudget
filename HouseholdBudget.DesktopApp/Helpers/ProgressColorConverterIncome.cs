696438e8a0b070c7ab92c2ee05f02f0c4b419d31
wooodiest <michalkuchnickiisc@gmail.com>
2025-06-03 02:48:53 +0200

    Better progress bar

--- HouseholdBudget.DesktopApp/Helpers/ProgressColorConverterIncome.cs
+++ HouseholdBudget.DesktopApp/Helpers/ProgressColorConverterIncome.cs
@@ -4,7 +4,7 @@ using System.Windows.Media;
 
 namespace HouseholdBudget.DesktopApp.Helpers
 {
-    public class ProgressColorConverter : IValueConverter
+    public class ProgressColorConverterExpense : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
@@ -15,6 +15,26 @@ namespace HouseholdBudget.DesktopApp.Helpers
                     < 0.5 => new SolidColorBrush(Colors.Green),
                     < 1.0 => new SolidColorBrush(Colors.Orange),
                     _ => new SolidColorBrush(Colors.Red)
+                };
+            }
+            return Brushes.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+
+    public class ProgressColorConverterIncome : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double progress)
+            {
+                return progress switch
+                {
+                    < 0.5 => new SolidColorBrush(Colors.Red),
+                    < 1.0 => new SolidColorBrush(Colors.Orange),
+                    _ => new SolidColorBrush(Colors.Green)
                 };
             }
             return Brushes.Gray;
